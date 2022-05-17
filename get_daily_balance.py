b66d2715fbee27d9d40df1b393cbe336ccaa488e
Portfedh <59422723+portfedh@users.noreply.github.com>
2022-05-17 14:31:27 -0500

    Refactor: Renamed files for simplicity

--- get_daily_balance.py
+++ get_daily_balance.py
@@ -1,29 +1,29 @@
-from scripts import daily_balance
-
-# GBM Account
-#############
-# Get monthly balance
-balance_df = daily_balance.create_balance_df('Inputs/CLG_GBM_Balances.csv')
-# Create daily balance
-daily_balance_df = daily_balance.create_daily_balance_df(balance_df, 'Tot_Cta_GBM_MXN')
-# Output to CSV
-daily_balance_df.to_csv("Outputs/balance_gbm_diario.csv", index=True, index_label='Date')
+from scripts import daily_account_balance as dab
 
 # CETES Account
 ###############
 # Get monthly balance
-balance_df2 = daily_balance.create_balance_df('Inputs/CLG_CETES_Balances.csv')
+balance_df2 = dab.create_balance_df('inputs/monthly_account_balance_CLG_CETES.csv')
 # Create daily balance
-daily_balance_df2 = daily_balance.create_daily_balance_df(balance_df2, 'Tot_Cta_Cetes_MXN')
+daily_balance_df2 = dab.create_daily_balance_df(balance_df2, 'Tot_Acct_Cetes_MXN')
 # Output to CSV
-daily_balance_df2.to_csv("Outputs/balance_cetes_diario.csv", index=True, index_label='Date')
+daily_balance_df2.to_csv("outputs/daily_acct_balance_CLG_CETES.csv", index=True, index_label='Date')
+
+# GBM Account
+#############
+# Get monthly balance
+balance_df = dab.create_balance_df('inputs/monthly_account_balance_CLG_GBM.csv')
+# Create daily balance
+daily_balance_df = dab.create_daily_balance_df(balance_df, 'Tot_Acct_GBM_MXN')
+# Output to CSV
+daily_balance_df.to_csv("outputs/daily_acct_balance_CLG_GBM.csv", index=True, index_label='Date')
 
 # Sum both Accounts
 ###################
 # Accounts
-cetes_acct = 'Outputs/balance_cetes_diario.csv'
-gbm_acct = 'Outputs/balance_gbm_diario.csv'
+cetes_acct = 'outputs/daily_acct_balance_CLG_CETES.csv'
+gbm_acct = 'outputs/daily_acct_balance_CLG_GBM.csv'
 # Get total daily balance
-total_balance_df = daily_balance.sum_total_daily_balance_df(cetes_acct, gbm_acct)
+total_balance_df = dab.sum_total_daily_balance_df(cetes_acct, gbm_acct)
 # Output to CSV
-total_balance_df.to_csv("Outputs/balance_total_diario.csv", index=False)
+total_balance_df.to_csv("outputs/daily_acct_balance_CLG_AllAccounts.csv", index=False)
--- scripts/daily_account_balance.py
+++ scripts/daily_account_balance.py
@@ -1,8 +1,8 @@
-# Functions to create the daily balance per account
+# Functions to create the daily balance for all bank accounts
 
 import pandas as pd
 import datetime as dt
-import analysis_dates
+import set_analysis_dates
 
 def create_balance_df(file_name: str) -> 'pd':
     """Create a dataframe from imported CSV file with monthly account balances.
@@ -10,7 +10,7 @@ def create_balance_df(file_name: str) -> 'pd':
     values are displayed as floats."""
     balance_df = pd.read_csv(file_name)
     # Create list from 'Date' column 
-    datetime_index_trades = pd.to_datetime(balance_df['Date'])
+    datetime_index_trades = pd.to_datetime(balance_df['Date'], dayfirst=True)
     # Make 'Date' column an index object
     datetime_index_trades = pd.DatetimeIndex(datetime_index_trades.values)
     # Append new 'Date' column to dataframe, as index
@@ -28,7 +28,7 @@ def create_daily_balance_df(balance_df:'pd', column_name:str) -> 'pd':
     values are displayed as integers."""
     # Create output dataframe
     daily_balance_df = pd.DataFrame(columns = [column_name])
-    for date in analysis_dates.date_range:
+    for date in set_analysis_dates.date_range:
         # Filter dataframe up to date
         filtered_blance_df = balance_df.loc[:date]
         # Get the last balance value
@@ -45,7 +45,7 @@ def create_daily_balance_df(balance_df:'pd', column_name:str) -> 'pd':
     daily_balance_df = daily_balance_df.rename_axis('Date', axis=1)
     return daily_balance_df
 
-### ToDo: Unlimited number of accounts ###
+### ToDo: Enable function to use more than two accounts ###
 def sum_total_daily_balance_df(file_name1: str, file_name2: str) -> 'pd':
     """Create a dataframe from imported CSV file with total account balances.
     requires a CSV file with data from 'create_daily_balance_df' function' as input.
@@ -59,7 +59,7 @@ def sum_total_daily_balance_df(file_name1: str, file_name2: str) -> 'pd':
     column_name = df_account2.columns[1]
     df_total[column_name] = df_account2[[column_name]].copy()
     # Add Total Values
-    df_total['Tot_Ctas_Portafolio_MXN'] = df_total[df_total.columns[1]]+ df_total[df_total.columns[2]]
+    df_total['Tot_Acct_Portafolio_MXN'] = df_total[df_total.columns[1]]+ df_total[df_total.columns[2]]
     # Drop other columns
     df_total.drop(columns=[df_total.columns[1], df_total.columns[2]], inplace=True)
     return df_total
