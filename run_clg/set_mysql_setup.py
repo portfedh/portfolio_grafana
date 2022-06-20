1f3944c6a2ed00edbacdc8141cda23a83ff94d82
Portfedh <59422723+portfedh@users.noreply.github.com>
2022-06-19 22:28:54 -0500

    Style: set_mysql_setup style changes

--- run_clg/set_mysql_setup.py
+++ run_clg/set_mysql_setup.py
@@ -19,15 +19,16 @@ mycursor.execute("USE CLG_database")
 # Create Tables:
 # dict = {table_name: column_name}
 
-# Create Table: Daily account balance, Daily contributions
+# Create INT Tables: Daily account balance, Daily contributions, IRR
 int_dict = {
     'daily_acct_balance_CLG_AllAccounts': 'Tot_Acct_Portafolio_MXN',
     'daily_acct_balance_CLG_CETES': 'Tot_Acct_Cetes_MXN',
     'daily_acct_balance_CLG_GBM': 'Tot_Acct_GBM_MXN',
-
     'daily_contributions_CLG_AllAccounts': 'Tot_Contribuciones_MXN',
     'daily_contributions_CLG_CETES': 'Contribuciones_Cetes_MXN',
     'daily_contributions_CLG_GBM': 'Contribuciones_GBM_MXN',
+    'irr_contributions_CLG_AllAccounts': 'Tot_Contribuciones_MXN',
+    'irr_monthly_account_balance_CLG_AllAccounts': 'Tot_Acct_Portafolio_MXN',
     }
 
 for x, y in int_dict.items():
@@ -36,11 +37,9 @@ for x, y in int_dict.items():
         f"CREATE TABLE {x} "
         f"(Date TIMESTAMP, {y} INT)")
 
-# Create Table: IRR
+# Create Float Table: XIRR
 float_dict = {
     'irr_xirr_CLG': 'XIRR',
-    'irr_contributions_CLG_AllAccounts': 'Tot_Contribuciones_MXN',
-    'irr_monthly_account_balance_CLG_AllAccounts': 'Tot_Acct_Portafolio_MXN',
     }
 
 for x, y in float_dict.items():
@@ -49,7 +48,7 @@ for x, y in float_dict.items():
         f"CREATE TABLE {x} "
         f"(Date TIMESTAMP, {y} FLOAT)")
 
-# Create Table: Returns
+# Create two column tables: Returns
 return_dict = {
     'returns_portfolio_CLG_AllAccounts':
     ['Tot_Portfolio_Return_MXN', 'Tot_Portfolio_Return_Percent']
