1a5f997ec31818547efc9a3f27f06343deff009f
Portfedh <59422723+portfedh@users.noreply.github.com>
2022-06-22 22:19:49 -0500

    Feat: Add get_returns.py to second user

---
+++ get_returns.py
@@ -0,0 +1,35 @@
+from sqlalchemy import create_engine
+from scripts import daily_balance as db
+from scripts import return_calculations as rc
+
+# MySQL Connection Settings
+###########################
+engine = create_engine(
+    'mysql+pymysql://root:password1@localhost:3306/PCL_database')
+
+# Create Portfolio Returns
+##########################
+returns_df = rc.returns(
+    contributions=db.create_df(
+        'outputs/daily_contributions_PCL_AllAccounts.csv'),
+    balance=db.create_df(
+        'outputs/daily_acct_balance_PCL_AllAccounts.csv'),
+    col_contrb='Tot_Contribuciones_MXN',
+    col_balance='Tot_Acct_Portafolio_MXN',
+    col_sub='Tot_Portfolio_Return_MXN',
+    col_ratio='Tot_Portfolio_Return_Percent')
+
+# Outputs
+#########
+# Save value to CSV
+returns_df.to_csv(
+    "outputs/returns_portfolio_PCL_AllAccounts.csv",
+    index=True, index_label='Date')
+
+# Save value to MySQL
+returns_mysql = db.create_df('outputs/returns_portfolio_PCL_AllAccounts.csv')
+returns_mysql.to_sql(
+    name='returns_portfolio_PCL_AllAccounts',
+    con=engine,
+    if_exists='replace',
+    index=True, index_label='Date')
