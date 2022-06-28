9d4c940376fdca41720d448c216a13832c96a986
Portfedh <59422723+portfedh@users.noreply.github.com>
2022-06-27 19:01:06 -0500

    Feat: Update .sh file to run all files

--- z_portfolio_run.sh
+++ z_portfolio_run.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Check who depends on set_analysis_dates
+# Check who used daily percentages
+
 echo "Cleaning outputs directory:"
 rm -v outputs/*
 
@@ -14,9 +17,6 @@ echo "Executing Portfolio Scripts:"
 echo "    - Executing set_mysql_setup."
 ./venv/bin/python3 set_mysql_setup.py
 
-#echo "    - Executing set_analysis_dates."
-#./venv/bin/python3 set_analysis_dates.py
-
 echo "    - Executing get_daily_balance."
 ./venv/bin/python3 get_daily_balance.py
 
@@ -26,19 +26,19 @@ echo "    - Executing get_daily_contributions."
 echo "    - Executing get_irr."
 ./venv/bin/python3 get_irr.py
 
-# echo "    - Executing get_returns."
-# ./venv/bin/python3 get_returns.py
+echo "    - Executing get_returns."
+./venv/bin/python3 get_returns.py
 
-# echo "    - Executing get_daily_shares."
-# ./venv/bin/python3 get_daily_shares.py
+echo "    - Executing get_daily_shares."
+./venv/bin/python3 get_daily_shares.py
 
-# echo "    - Executing get_daily_prices."
-# ./venv/bin/python3 get_daily_prices.py
+echo "    - Executing get_daily_prices."
+./venv/bin/python3 get_daily_prices.py
 
-# echo "    - Executing get_daily_subtotals."
-# ./venv/bin/python3 get_daily_subtotals.py
+echo "    - Executing get_daily_subtotals."
+./venv/bin/python3 get_daily_subtotals.py
 
-# echo "    - Executing get_daily_percentages."
-# ./venv/bin/python3 get_daily_percentages.py
+echo "    - Executing get_daily_percentages."
+./venv/bin/python3 get_daily_percentages.py
 
 echo "All scripts executed successfully."
