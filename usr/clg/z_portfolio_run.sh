1e1d6ff3ca3868c1d470548dda524d44b5e60d8a
Portfedh <59422723+portfedh@users.noreply.github.com>
2022-07-09 19:46:49 -0500

    Fix: Change venv path to linux computer

--- usr/clg/z_portfolio_run.sh
+++ usr/clg/z_portfolio_run.sh
@@ -11,30 +11,30 @@ echo
 echo "Executing Portfolio Scripts:"
 
 echo "    - Executing set_mysql_setup."
-./venv/bin/python3 set_mysql_setup.py
+./venv_linux/bin/python3 set_mysql_setup.py
 
 echo "    - Executing get_daily_balance."
-./venv/bin/python3 get_daily_balance.py
+./venv_linux/bin/python3 get_daily_balance.py
 
 echo "    - Executing get_daily_contributions."
-./venv/bin/python3 get_daily_contributions.py
+./venv_linux/bin/python3 get_daily_contributions.py
 
 echo "    - Executing get_irr."
-./venv/bin/python3 get_irr.py
+./venv_linux/bin/python3 get_irr.py
 
 echo "    - Executing get_returns."
-./venv/bin/python3 get_returns.py
+./venv_linux/bin/python3 get_returns.py
 
 echo "    - Executing get_daily_shares."
-./venv/bin/python3 get_daily_shares.py
+./venv_linux/bin/python3 get_daily_shares.py
 
 echo "    - Executing get_daily_prices."
-./venv/bin/python3 get_daily_prices.py
+./venv_linux/bin/python3 get_daily_prices.py
 
 echo "    - Executing get_daily_subtotals."
-./venv/bin/python3 get_daily_subtotals.py
+./venv_linux/bin/python3 get_daily_subtotals.py
 
 echo "    - Executing get_daily_percentages."
-./venv/bin/python3 get_daily_percentages.py
+./venv_linux/bin/python3 get_daily_percentages.py
 
 echo "All scripts executed successfully."
