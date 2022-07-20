6c4e9891ef4e114b83f595e7ab4647bb77922101
Portfedh <59422723+portfedh@users.noreply.github.com>
2022-07-19 22:16:45 -0500

    Feat: Add IBKR Account

--- get_daily_balance.py
+++ get_daily_balance.py
@@ -1,36 +1,35 @@
 from sqlalchemy import create_engine
 from scripts import daily_balance as db
 
-# MySQL Connection Settings
-###########################
+# Connect to MySQL database
+##############################################################################
 url = 'mysql+pymysql://root:password1@localhost:3306/PCL_database'
 engine = create_engine(url)
 
-"""
-# IBKR Account
-###############
+# GBM Account
+##############################################################################
 # Get monthly balance
-balance_df3 = db.create_df('inputs/pcl/monthly_account_balance_PCL_IBKR.csv')
+balance_df1 = db.create_df('inputs/pcl/monthly_account_balance_PCL_GBM.csv')
 
 # Create daily balance
-daily_balance_df3 = db.daily_balance(
-    df=balance_df3,
-    column_name='Tot_Acct_IBKR_MXN',
+daily_balance_df1 = db.daily_balance(
+    df=balance_df1,
+    column_name='Tot_Acct_GBM_MXN',
     sum=False)
 
 # Output to CSV
-filename1 = 'outputs/daily_acct_balance_PCL_IBKR.csv'
-daily_balance_df3.to_csv(filename1, index=True, index_label='Date')
+filename1 = 'outputs/daily_acct_balance_PCL_GBM.csv'
+daily_balance_df1.to_csv(filename1, index=True, index_label='Date')
 
 # Output to MySQL
-daily_balance_df3.to_sql(
-    name='daily_acct_balance_PCL_IBKR',  # Table name
+daily_balance_df1.to_sql(
+    name='daily_acct_balance_PCL_GBM',  # Table name
     con=engine,
     if_exists='replace',
     index=True, index_label='Date')
-"""
+
 # CETES Account
-###############
+##############################################################################
 # Get monthly balance
 balance_df2 = db.create_df('inputs/pcl/monthly_account_balance_PCL_CETES.csv')
 
@@ -41,8 +40,8 @@ daily_balance_df2 = db.daily_balance(
     sum=False)
 
 # Output to CSV
-filename1 = 'outputs/daily_acct_balance_PCL_CETES.csv'
-daily_balance_df2.to_csv(filename1, index=True, index_label='Date')
+filename2 = 'outputs/daily_acct_balance_PCL_CETES.csv'
+daily_balance_df2.to_csv(filename2, index=True, index_label='Date')
 
 # Output to MySQL
 daily_balance_df2.to_sql(
@@ -51,42 +50,44 @@ daily_balance_df2.to_sql(
     if_exists='replace',
     index=True, index_label='Date')
 
-# GBM Account
-#############
+# IBKR Account
+##############################################################################
 # Get monthly balance
-balance_df = db.create_df('inputs/pcl/monthly_account_balance_PCL_GBM.csv')
+balance_df3 = db.create_df('inputs/pcl/monthly_account_balance_PCL_IBKR.csv')
 
 # Create daily balance
-daily_balance_df = db.daily_balance(
-    df=balance_df,
-    column_name='Tot_Acct_GBM_MXN',
+daily_balance_df3 = db.daily_balance(
+    df=balance_df3,
+    column_name='Tot_Acct_IBKR_MXN',
     sum=False)
 
 # Output to CSV
-filename2 = 'outputs/daily_acct_balance_PCL_GBM.csv'
-daily_balance_df.to_csv(filename2, index=True, index_label='Date')
+filename3 = 'outputs/daily_acct_balance_PCL_IBKR.csv'
+daily_balance_df3.to_csv(filename3, index=True, index_label='Date')
 
 # Output to MySQL
-daily_balance_df.to_sql(
-    name='daily_acct_balance_PCL_GBM',  # Table name
+daily_balance_df3.to_sql(
+    name='daily_acct_balance_PCL_IBKR',  # Table name
     con=engine,
     if_exists='replace',
     index=True, index_label='Date')
 
-# Sum both Accounts
-###################
-# Get total daily balance
-total_balance_df = db.consolidate(
-    file_name_1='outputs/daily_acct_balance_PCL_CETES.csv',
-    file_name_2='outputs/daily_acct_balance_PCL_GBM.csv',
-    sum_col_name='Tot_Acct_Portafolio_MXN')
+# Sum All Accounts
+##############################################################################
+acct_1 = 'outputs/daily_acct_balance_PCL_CETES.csv'
+acct_2 = 'outputs/daily_acct_balance_PCL_GBM.csv'
+acct_3 = 'outputs/daily_acct_balance_PCL_IBKR.csv'
+
+added_df = db.add_df(acct_1, acct_2, acct_3)
+unique_df = db.remove_duplicates(added_df)
+total_balance_df = db.add_total_column(unique_df, 'Tot_Acct_Portafolio_MXN')
 
 # Output to CSV
-filename3 = 'outputs/daily_acct_balance_PCL_AllAccounts.csv'
-total_balance_df.to_csv(filename3, index=False)
+filename4 = 'outputs/daily_acct_balance_PCL_AllAccounts.csv'
+total_balance_df.to_csv(filename4, index=False)
 
 # Output to MySQL
-total_balance_df2 = db.create_df(filename3)
+total_balance_df2 = db.create_df(filename4)
 
 total_balance_df2.to_sql(
     name='daily_acct_balance_PCL_AllAccounts',
