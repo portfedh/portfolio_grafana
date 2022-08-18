303b2142d654dd5a47c76a289c21a00b5d6b20cd
Portfedh <59422723+portfedh@users.noreply.github.com>
2022-08-18 16:24:03 -0500

    Refactor: Simplify function for get_returns.py

--- get_returns.py
+++ get_returns.py
@@ -6,21 +6,45 @@ from scripts import return_calculations as rc
 
 # Create Portfolio Returns
 ##########################
-returns_df = rc.returns(
-    contributions=db.create_df(
-        'outputs/daily_contributions_CLG_AllAccounts.csv'),
-    balance=db.create_df(
-        'outputs/daily_acct_balance_CLG_AllAccounts.csv'),
-    col_contrb='Tot_Contribuciones_MXN',
-    col_balance='Tot_Acct_Portafolio_MXN',
-    col_sub='Tot_Portfolio_Return_MXN',
-    col_ratio='Tot_Portfolio_Return_Percent')
+
+# Variables
+contributions = db.create_df(
+    'outputs/daily_contributions_CLG_AllAccounts.csv')
+balance = db.create_df(
+    'outputs/daily_acct_balance_CLG_AllAccounts.csv')
+col_contrb = 'Tot_Contribuciones_MXN'
+col_balance = 'Tot_Acct_Portafolio_MXN'
+col_return = 'Tot_Portfolio_Return_MXN'
+col_ratio = 'Tot_Portfolio_Return_Percent'
+
+# Merge daily contributions and daily balance dataframes
+return_df = rc.merge_df(contributions, balance)
+
+# Create column with the portfolio return $
+return_df = rc.subtract_column(
+    df=return_df,
+    column1=col_balance,
+    column2=col_contrb,
+    subtraction_col=col_return)
+
+# Save column data as Integer
+return_df = rc.df_column_to_int(return_df, col_return)
+
+# Create column with the portfolio return %
+return_df = rc.add_ratio_column(
+    df=return_df,
+    column_name=col_ratio,
+    column2=col_balance,
+    column1=col_contrb)
+
+# Drop calculation columns
+return_df = rc.drop_column(return_df, col_contrb, col_balance)
 
 # Outputs
 #########
 # Save value to CSV
 filename = 'outputs/returns_portfolio_CLG_AllAccounts.csv'
-returns_df.to_csv(filename, index=True, index_label='Date')
+return_df.to_csv(filename, index=True, index_label='Date')
 
 # Save value to MySQL
 returns_mysql = db.create_df(filename)
--- scripts/return_calculations.py
+++ scripts/return_calculations.py
@@ -10,6 +10,7 @@ The module contains the following functions:
 import pandas as pd
 
 
+#  To Delete #################################################################
 def returns(
         contributions: 'pd',
         balance: 'pd',
@@ -74,3 +75,39 @@ def returns(
         inplace=True)
     # Return dataframe
     return returns
+#  End To Delete #############################################################
+
+
+#  New Functions #############################################################
+
+def merge_df(dataframe1, dataframe2):
+    ''' Merge dataframe2 into dataframe1'''
+    merged_df = dataframe1.merge(
+        dataframe2, left_index=True, right_index=True)
+    return merged_df
+
+
+def subtract_column(df, column1, column2, subtraction_col):
+    ''' Create column with the subtraction of column2 from column1'''
+    df[subtraction_col] = (df[column1] - df[column2])
+    return df
+
+
+def df_column_to_int(df, column):
+    ''' Transform column data to Integers '''
+    df[column] = (df[column].astype('int'))
+    return df
+
+
+def add_ratio_column(df, column_name, column2, column1):
+    ''' Create column with the ratio of column2 / column1 '''
+    df[column_name] = ((df[column2] / df[column1])-1)
+    return df
+
+
+def drop_column(df, *args):
+    ''' Drop columns from dataframe '''
+    df.drop(list(args), axis=1, inplace=True)
+    return df
+
+#  End New Functions #########################################################
