fa2f0de15c3f6af31e7731640b871a858bf2cf1b
Portfedh <59422723+portfedh@users.noreply.github.com>
2022-08-18 22:41:45 -0500

    Docs: Added documentation to functions

--- scripts/irr_calculations.py
+++ scripts/irr_calculations.py
@@ -3,25 +3,42 @@
 Functions used to calculate the IRR of the portfolio.
 
 Uses consolidated contributions file and the last value in
-the consolidated account balance file. 
+the consolidated account balance file as inputs.
 
 The module contains the following functions:
 
 Irr contributions functions:
     - concat_df(*args)
+        Concatenate unlimited dataframes
+
     - to_datetime_df(df, date_column)
+        Turn date to datetime format
+
     - add_total_df(df, col_name)
+        Add total column to df
+
     - filter_df(df: pd, columns: list
+        Filter df to keep columns in list
+
     - invert_cf_df(df, column_name)
+        Invert values as cashflows
+
     - integers_df(df, column_name)
+        Save values as integers
 
 Monthly balance functions:
     - merge_df(*args: pd)
+        Merge unlimited dataframes
 
 IRR Calculations:
     - get_last_value(df)
+        Get las value from a pandas dataframe
+
     - rename_column(df, balance_column, contributions_column)
+        Rename the balance column like the contributions column
+
     - split_df(df, contributions_column)
+        Separate the dataframe into two lists for the xirr function
 """
 
 import pandas as pd
@@ -29,7 +46,7 @@ import pandas as pd
 
 # irr_contributions functions
 ##############################################################################
-def concat_df(*args):
+def concat_df(*args: pd) -> pd:
     """Concatenate unlimited dataframes"""
     list = []
     for x in args:
@@ -43,7 +60,7 @@ def concat_df(*args):
 
 
 # Like daily_balance but does not require csv
-def to_datetime_df(df, date_column):
+def to_datetime_df(df: pd, date_column: pd.column) -> pd:
     """Turn date to datetime format"""
     datetime_date = pd.to_datetime(df[date_column], dayfirst=True)
     datetime_index_trades = pd.DatetimeIndex(datetime_date.values)
@@ -53,7 +70,7 @@ def to_datetime_df(df, date_column):
     return df
 
 
-def add_total_df(df, col_name):
+def add_total_df(df: pd, col_name: pd.column) -> pd:
     """Add total column to df"""
     df[col_name] = df.sum(axis=1)
     return df
@@ -61,13 +78,13 @@ def add_total_df(df, col_name):
 
 # Check if column variable can be changed to a string
 # df = df.filter([columns])
-def filter_df(df: pd, columns: list):
+def filter_df(df: pd, columns: list) -> pd:
     "Filter df to keep columns in list"
     df = df.filter(columns)
     return df
 
 
-def invert_cf_df(df, column_name):
+def invert_cf_df(df: pd, column_name: str) -> pd:
     """Invert values as cashflows"""
     df[column_name] = df[column_name]*-1
     return df
@@ -76,7 +93,7 @@ def invert_cf_df(df, column_name):
 # Can be eliminated if dataframe has date as index
 # Simply run:
 # df = df.astype('int')
-def integers_df(df, column_name):
+def integers_df(df: pd, column_name: str) -> pd:
     """Save values as integers"""
     df[column_name] = df[column_name].astype('int')
     return df
@@ -86,7 +103,7 @@ def integers_df(df, column_name):
 ##############################################################################
 
 # Very similar to concat_df but with axis=1 and without read CSV
-def merge_df(*args: pd):
+def merge_df(*args: pd) -> pd:
     """Merge unlimited dataframes"""
     list = []
     for x in args:
@@ -99,19 +116,19 @@ def merge_df(*args: pd):
 # IRR Calculations
 ##############################################################################
 
-def get_last_value(df):
+def get_last_value(df: pd) -> pd:
     """Get las value from a pandas dataframe"""
     df = df.iloc[-1:]
     return df
 
 
-def rename_column(df, balance_column, contributions_column):
+def rename_column(df: pd, balance_col: str, contributions_col: str) -> pd:
     """Rename the balance column like the contributions column"""
-    df = df.rename(columns={balance_column: contributions_column})
+    df = df.rename(columns={balance_col: contributions_col})
     return df
 
 
-def split_df(df, contributions_column):
+def split_df(df: pd, contributions_column: str) -> pd:
     """Separate the dataframe into two lists for the xirr function"""
     date_list = df.index.tolist()
     values_list = list(df[contributions_column])
--- scripts/return_calculations.py
+++ scripts/return_calculations.py
@@ -1,113 +1,64 @@
 # return_calculations.py
-"""Calculates a simple return for the portfolio in $ and % amounts.
+"""Calculates the return for the portfolio in $ and % amounts.
 
 The module contains the following functions:
 
-- returns(contributions, balance, col_contrb, col_balance, col_sub, col_ratio):
-    Calculates a simple return for the portfolio in $ ammount and % ammount.
+- merge_df(dataframe1, dataframe2):
+    Merge dataframe2 into dataframe1
+
+- subtract_column(df, column1, column2, subtraction_col):
+    Create column with the subtraction of column2 from column1
+
+- df_column_to_int(df, column):
+    Transform column data to integers
+
+- add_ratio_column(df, column_name, column2, column1):
+    Create column with the ratio of column2 / column1
+
+- def drop_column(df: pd, *args: pd.column) -> pd:
+    Drop columns from dataframe
+
 """
 
 import pandas as pd
 
 
-#  To Delete #################################################################
-def returns(
-        contributions: 'pd',
-        balance: 'pd',
-        col_contrb: str,
-        col_balance: str,
-        col_sub: str,
-        col_ratio: str
-        ) -> 'pd':
-    """
-    Calculates a simple return for the portfolio in $ and % amounts.
-
-    Calculations are as follows:
-        Return $ = (Balance - Contributions).
-        Return % = (Balance / Contributions) - 1.
-
-        Parameters:
-            contributions:
-                - Consolidated daily contributions dataframe.
-
-            balance:
-                - Consolidated daily account balance dataframe.
-
-            col_contrb:
-                - Column name from file 1 with values.
-
-            col_balance:
-                - Column name from file 2 with values.
-
-            col_sub:
-                - Column name for subtraction results:
-                    (col_name2 - col_name1).
-
-            col_ratio:
-                - Column name for ratio results:
-                    (col_name2 / col_name1) - 1.
-
-        Returns:
-            returns:
-                - Dataframe with 3 columns:
-                    'Date' column as index in datetime format.
-                    col_sub: int. Product of (balance - contributions).
-                    col_ratio: float. Ratio of (balance / contributions)-1.
-    """
-    # Merge two files
-    returns = contributions.merge(
-        balance,
-        left_index=True,
-        right_index=True)
-    # Add Column with the subtraction of column2 from column1
-    returns[col_sub] = (
-        returns[col_balance] - returns[col_contrb])
-    # Save as Integer
-    returns[col_sub] = (
-        returns[col_sub].astype('int'))
-    # Add Column with the ratio of column2 from column1
-    returns[col_ratio] = (
-        (returns[col_balance] / returns[col_contrb])-1)
-    # Drop individual columns
-    returns.drop(
-        [col_contrb, col_balance],
-        axis=1,
-        inplace=True)
-    # Return dataframe
-    return returns
-#  End To Delete #############################################################
-
-
-#  New Functions #############################################################
-
-def merge_df(dataframe1, dataframe2):
+def merge_df(dataframe1: pd, dataframe2: pd) -> pd:
     ''' Merge dataframe2 into dataframe1'''
     merged_df = dataframe1.merge(
         dataframe2, left_index=True, right_index=True)
     return merged_df
 
 
-def subtract_column(df, column1, column2, subtraction_col):
+def subtract_column(
+        df: pd,
+        column1: pd.column,
+        column2: pd.column,
+        subtraction_col: pd.column,
+        ) -> pd:
     ''' Create column with the subtraction of column2 from column1'''
     df[subtraction_col] = (df[column1] - df[column2])
     return df
 
 
-def df_column_to_int(df, column):
-    ''' Transform column data to Integers '''
+def df_column_to_int(df: pd, column: pd.column) -> pd:
+    ''' Transform column data to integers '''
     df[column] = (df[column].astype('int'))
     return df
 
 
-def add_ratio_column(df, column_name, column2, column1):
+def add_ratio_column(
+        df: pd,
+        column_name: str,
+        column2: pd.column,
+        column1: pd.column
+        ) -> pd:
     ''' Create column with the ratio of column2 / column1 '''
     df[column_name] = ((df[column2] / df[column1])-1)
     return df
 
 
-def drop_column(df, *args):
+def drop_column(df: pd, *args: pd.column) -> pd:
     ''' Drop columns from dataframe '''
     df.drop(list(args), axis=1, inplace=True)
     return df
-
-#  End New Functions #########################################################
